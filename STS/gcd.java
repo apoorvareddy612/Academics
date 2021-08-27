b03df41d377040d69860e6a6f1888ddfc7db9554
Apoorva Reddy <61325120+apoorvareddy612@users.noreply.github.com>
2021-08-27 13:06:37 +0530

    Update and rename sts1.java to gcd.java

---
+++ STS/gcd.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class sts1 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x,y;
+        int gcd = 1;
+        System.out.println("Enter a value: ");
+        x = sc.nextInt();
+        System.out.println("You have given "+ x);
+        System.out.println("Enter another value: ");
+        y = sc.nextInt();
+        System.out.println("You have given "+ y);
+
+        for(int i = 1;i <=x && i <=y; i++)
+        {
+            if(x%i==0 && y%i==0)
+            {
+                gcd = i;
+            }
+
+        }
+
+       
+    } 
+
+
+    
+}
